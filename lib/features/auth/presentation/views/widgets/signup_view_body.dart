83b987abc98e56bccb44e37bf00050122a2a99dc
Nady Alwazah <nadysy.7@gmail.com>
2026-05-19 19:47:46 +0300

    feat(auth/signup): build signup form UI using CustomTextFormField and FormLabel

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task05_skills_academy_beg/core/theme/app_colors.dart';
 import 'package:flutter_task05_skills_academy_beg/core/utils/styles.dart';
+import 'package:flutter_task05_skills_academy_beg/features/auth/presentation/views/widgets/custom_text_form_field.dart';
 import 'package:flutter_task05_skills_academy_beg/features/auth/presentation/views/widgets/form_label.dart';
 
 class SignupViewBody extends StatefulWidget {
@@ -26,12 +27,39 @@ class _SignupViewBodyState extends State<SignupViewBody> {
                 "Create Account",
                 style: Styles.heading1.copyWith(color: AppColors.primary),
               ),
+
               const Text(
                 "Start your journey with professional-grade learning.",
                 style: TextStyle(color: Color(0xFF2F2F39)),
               ),
+
               const SizedBox(height: 16),
+
               const FormLabel(text: "Full Name"),
+
+              const CustomTextFormField(
+                hintText: "Enter your full name",
+                textInputType: TextInputType.text,
+              ),
+
+              const SizedBox(height: 8),
+
+              const FormLabel(text: "Email Address"),
+
+              const CustomTextFormField(
+                hintText: "name@example.com",
+                textInputType: TextInputType.text,
+              ),
+
+              const SizedBox(height: 8),
+
+              const FormLabel(text: "Password"),
+
+              const CustomTextFormField(
+                hintText: "Min. 8 characters ",
+                textInputType: TextInputType.text,
+                obscureText: true,
+              ),
             ],
           ),
         ),
