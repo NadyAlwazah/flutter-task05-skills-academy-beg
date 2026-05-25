8fe259a4899050fa4b80a59fbb58696b9e8acb0e
Nady Alwazah <nadysy.7@gmail.com>
2026-05-26 02:25:24 +0300

    feat(auth): make 'Log In' text clickable using GestureDetector for improved UX

--- lib/features/auth/presentation/views/widgets/have_an_account_widget.dart
+++ lib/features/auth/presentation/views/widgets/have_an_account_widget.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
-
 import '../../../../../core/utils/styles.dart';
 
 class HaveAnAccountWidget extends StatelessWidget {
-  const HaveAnAccountWidget({super.key});
+  final VoidCallback? onLoginTap;
+
+  const HaveAnAccountWidget({super.key, this.onLoginTap});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text.rich(
-          TextSpan(
-            children: [
-              TextSpan(
-                text: "Already have an account? ",
-                style: Styles.authText,
-              ),
-              TextSpan(text: "Log In ", style: Styles.authAction),
-            ],
-          ),
+        const Text("Already have an account? ", style: Styles.authText),
+        GestureDetector(
+          onTap: onLoginTap,
+          child: const Text("Log In", style: Styles.authAction),
         ),
       ],
     );
